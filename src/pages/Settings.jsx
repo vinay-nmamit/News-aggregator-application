b71f6882f127e808109b24a2cb821e696f596734
vinay-nmamit <vk5022702@gmail.com>
2025-03-20 11:45:11 +0530

    added settings page

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,11 @@
+function Settings() {
+    return (
+      <div>
+        <h1>Settings</h1>
+        <p>Configure your settings here.</p>
+      </div>
+    );
+  }
+  
+  export default Settings;
+  
